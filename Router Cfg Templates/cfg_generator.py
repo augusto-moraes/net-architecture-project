5fabf8c992b14c34338fa5682f4cb41fc26d2777
boris <boris.testud@etu.univ-grenoble-alpes.fr>
2023-03-29 08:19:50 +0200

    Automatisation V1

--- Router Cfg Templates/cfg_generator.py
+++ Router Cfg Templates/cfg_generator.py
@@ -2,15 +2,20 @@ import jinja2
 import json
 import os
 
-def main():
 
-    template_file = "router.j2"
-    json_parameter_file = "info.json"
+def createCfg():
+    template_file_PE = "PE.j2"
+    template_file_P = "P.j2"
+    json_parameter_file_PE = "infoPE.json"
+    json_parameter_file_P = "infoP.json"
     output_directory = "_output"
 
     # read the contents from the JSON files
     print("Read JSON parameter file...")
-    config_parameters = json.load(open(json_parameter_file))
+
+    config_parameters_PE = json.load(open(json_parameter_file_PE))
+    config_parameters_P = json.load(open(json_parameter_file_P))
+
 
     # next we need to create the central Jinja2 environment and we will load
     # the Jinja2 template file (the two parameters ensure a clean output in the
@@ -19,7 +24,8 @@ def main():
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath="."),
                              trim_blocks=True,
                              lstrip_blocks=True)
-    template = env.get_template(template_file)
+    template_PE = env.get_template(template_file_PE)
+    template_P = env.get_template(template_file_P)
 
     # we will make sure that the output directory exists
     if not os.path.exists(output_directory):
@@ -27,13 +33,25 @@ def main():
 
     # now create the templates
     print("Create templates...")
-    for parameter in config_parameters:
-        result = template.render(parameter)
+
+    for parameter in config_parameters_PE:
+        result = template_PE.render(parameter)
         f = open(os.path.join(output_directory, parameter['hostname'] + ".config"), "w")
         f.write(result)
         f.close()
         print("Configuration '%s' created..." % (parameter['hostname'] + ".config"))
+    for parameter in config_parameters_P:
+        result = template_P.render(parameter)
+        f = open(os.path.join(output_directory, parameter['hostname'] + ".config"), "w")
+        f.write(result)
+        f.close()
+        print("Configuration '%s' created..." % (parameter['hostname'] + ".config"))
+
+
     print("DONE")
+
+def main():
+    createCfg()
 
 if __name__ == "__main__":
     main()
